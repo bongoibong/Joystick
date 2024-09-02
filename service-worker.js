f3bbd43de3d9c12ca82989d123c134dc979d74b4
bongoibong <135612426+bongoibong@users.noreply.github.com>
2024-09-02 12:28:00 +0000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,22 @@
-const CACHE_NAME = 'todo-list-cache-v1';
-const urlsToCache = [
-  '/',
-  '/styles.css',
-  '/script.js',
-  '/manifest.json'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
-  );
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open('joystick-cache').then(function(cache) {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/manifest.json',
+                '/service-worker.js',
+                '/icon-192x192.png',
+                '/icon-512x512.png'
+            ]);
+        })
+    );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
-  );
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        })
+    );
 });
